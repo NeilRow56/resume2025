fd2e2521239f1c571104a2cf2414027d4b9fd0fc
NeilRow56 <neilrowland56@gmail.com>
2025-03-06 09:41:55 +0000

    Updated resume page with ResumeItem component to displat resume preview

--- src/app/(main)/resumes/ResumeItem.tsx
+++ src/app/(main)/resumes/ResumeItem.tsx
@@ -1,5 +1,139 @@
-import React from "react";
+"use client";
 
-export default function ResumeItem() {
-  return <div>ResumeItem</div>;
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { ResumeServerData } from "@/lib/types";
+import { formatDate } from "date-fns";
+import { MoreVertical, Printer, Trash2 } from "lucide-react";
+import Link from "next/link";
+import { useState, useTransition } from "react";
+
+interface ResumeItemProps {
+  resume: ResumeServerData;
+}
+
+export default function ResumeItem({ resume }: ResumeItemProps) {
+  const wasUpdated = resume.updatedAt !== resume.createdAt;
+
+  return (
+    <div className="group relative rounded-lg border border-transparent bg-secondary p-3 transition-colors hover:border-border">
+      <div className="space-y-3">
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="inline-block w-full text-center"
+        >
+          <p className="line-clamp-1 font-semibold">
+            {resume.title || "No title"}
+          </p>
+          {resume.description && (
+            <p className="line-clamp-2 text-sm">{resume.description}</p>
+          )}
+          <p className="text-xs text-muted-foreground">
+            {wasUpdated ? "Updated" : "Created"} on{" "}
+            {formatDate(resume.updatedAt, "MMM d, yyyy h:mm a")}
+          </p>
+        </Link>
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="relative inline-block w-full"
+        >
+          RESUME PREVIEW
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+interface MoreMenuProps {
+  resumeId: string;
+  onPrintClick: () => void;
+}
+
+function MoreMenu({ resumeId, onPrintClick }: MoreMenuProps) {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute right-0.5 top-0.5 opacity-0 transition-opacity group-hover:opacity-100"
+          >
+            <MoreVertical className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={() => setShowDeleteConfirmation(true)}
+          >
+            <Trash2 className="size-4" />
+            Delete
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={onPrintClick}
+          >
+            <Printer className="size-4" />
+            Print
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteConfirmationDialog
+        resumeId={resumeId}
+        open={showDeleteConfirmation}
+        onOpenChange={setShowDeleteConfirmation}
+      />
+    </>
+  );
+}
+
+interface DeleteConfirmationDialogProps {
+  resumeId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+function DeleteConfirmationDialog({
+  resumeId,
+  open,
+  onOpenChange,
+}: DeleteConfirmationDialogProps) {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete resume?</DialogTitle>
+          <DialogDescription>
+            This will permanently delete this resume. This action cannot be
+            undone.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          LOADING BUTTON
+          <Button variant="secondary" onClick={() => onOpenChange(false)}>
+            Cancel
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,26 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db/prisma";
 import { resumeDataInclude } from "@/lib/types";
 import { auth } from "@clerk/nextjs/server";
-import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
-import Link from "next/link";
+import ResumeItem from "./ResumeItem";
+import { prisma } from "@/lib/db/prisma";
 
 export const metadata: Metadata = {
   title: "Your resumes",
 };
 
-export default async function ResumePage() {
+export default async function ResumesPage() {
   const { userId } = await auth();
 
   if (!userId) {
     return null;
   }
+  // We use the Promise.all so that we can get the count and resumes at the same time.
 
   const [resumes, totalCount] = await Promise.all([
     prisma.resume.findMany({
       where: {
-        userId,
+        userId: userId,
       },
       orderBy: {
         updatedAt: "desc",
@@ -29,27 +28,23 @@ export default async function ResumePage() {
     }),
     prisma.resume.count({
       where: {
-        userId,
+        userId: userId,
       },
     }),
   ]);
+
+  //TODO: check quota for non premium users
+
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New resume
-        </Link>
-      </Button>
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your resumes</h1>
         <p>Total: {totalCount}</p>
       </div>
       <div className="flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4">
-        <div className="flex flex-col">
-          <span className="font-bold text-xl">Resume Summary Heading</span>
-          {resumes.map((resume) => JSON.stringify(resume.summary))}
-        </div>
+        {resumes.map((resume) => (
+          <ResumeItem key={resume.id} resume={resume} />
+        ))}
       </div>
     </main>
   );
